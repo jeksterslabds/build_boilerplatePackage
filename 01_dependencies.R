0f242d3534f33ebc1d17d649f7913e8b24533db0
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-22 20:53:12 +0800

    Devtools to remotes.

--- 01_dependencies.R
+++ 01_dependencies.R
@@ -1,9 +1,25 @@
+#' ---
+#' title: "Install Dependencies before Creating boilerplatePackage"
+#' author: "Ivan Jacob Agaloos Pesigan"
+#' date: "`r Sys.Date()`"
+#' output:
+#'   rmarkdown::github_document:
+#'     toc: true
+#' ---
+#'
+#+ setup
 repos <- "https://cran.rstudio.org"
-if (!require("devtools")) {
-  install.packages("devtools", repos = repos)
+if (!require("remotes")) {
+  install.packages(
+    "remotes",
+    repos = repos
+  )
 }
 if (!require("rmarkdown")) {
-  install.packages("rmarkdown", repos = repos)
+  install.packages(
+    "rmarkdown",
+    repos = repos
+  )
 }
 remotes::install_github(
   repo = "jeksterslabds/jeksterslabRpkg",
